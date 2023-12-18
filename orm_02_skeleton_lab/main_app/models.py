4cb1596e8c75d6aaf4840b15e94b826bcb9837de
StanDobrev11 <dobrev81@gmail.com>
2023-12-18 17:22:21 +0000

    update

--- orm_02_skeleton_lab/main_app/models.py
+++ orm_02_skeleton_lab/main_app/models.py
@@ -11,6 +11,9 @@ class Employee(models.Model):
     works_full_time = models.BooleanField()
     created_on = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return f"{self.name} - {self.email_address}"
+
 
 class Department(models.Model):
     LOCATION_CHOICES = [
@@ -25,3 +28,20 @@ class Department(models.Model):
     employees_count = models.PositiveIntegerField(default=1, verbose_name="Employees Count")
     location = models.CharField(max_length=20, choices=LOCATION_CHOICES, blank=True)
     last_edited_on = models.DateTimeField(auto_now=True, editable=False)
+
+    def __str__(self):
+        return f"{self.name} - {self.employees_count}"
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    budget = models.DecimalField(null=True, decimal_places=2, max_digits=10)
+    duration_in_days = models.PositiveIntegerField(null=True, verbose_name="Duration in Days")
+    estimated_hours = models.FloatField(null=True, verbose_name="Estimated Hours")
+    start_date = models.DateField(verbose_name="Start Date", auto_now_add=True, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_edited_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.name}"
