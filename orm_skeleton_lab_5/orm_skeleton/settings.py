acd1a0d0aae72710d7678543e5ca9a21b67a3b55
StanDobrev11 <dobrev81@gmail.com>
2023-12-22 19:35:31 +0000

    update

--- orm_skeleton_lab_5/orm_skeleton/settings.py
+++ orm_skeleton_lab_5/orm_skeleton/settings.py
@@ -127,3 +127,23 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler'}
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG', # other levels: CRITICAL, ERROR, WARNING, INFO, DEBUG
+    },
+    'loggers': {
+        'django.db.backends': { # responsible for the SQL logs
+            'handlers': ['console'],
+            'level': 'DEBUG',
+            'propagate': False,
+        }
+    }
+}
