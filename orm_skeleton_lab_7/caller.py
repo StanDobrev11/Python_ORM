78ba80a732af41a901f784d3acdf78a8e9fa8613
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-01-10 21:13:41 +0200

    update

--- orm_skeleton_lab_7/caller.py
+++ orm_skeleton_lab_7/caller.py
@@ -8,6 +8,12 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models
-from main_app.models import Animal, Mammal, Bird, Reptile
+from main_app.models import Animal, Mammal, Bird, Reptile, ZooKeeper, Veterinarian, ZooDisplayAnimal
+
 # Create and check models
 # Run and print your queries
+# keep the data from the previous exercise, so you can reuse it
+
+zookeeper = ZooKeeper(first_name="John", last_name="Doe", phone_number="0123456789", specialty="Fishes")
+zookeeper.full_clean()
+zookeeper.save()
--- orm_skeleton_lab_7/main_app/models.py
+++ orm_skeleton_lab_7/main_app/models.py
@@ -1,5 +1,3 @@
-from abc import ABC
-
 from django.db import models
 
 
@@ -23,22 +21,38 @@ class Reptile(Animal):
     scale_type = models.CharField(max_length=50)
 
 
-class Employee(models.Model, ABC):
+class Employee(models.Model):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     phone_number = models.CharField(max_length=10)
 
+    class Meta:
+        abstract = True
+
 
 class ZooKeeper(Employee):
-    SPECS = (
-        ("Mammals", "Mammals"),
-        ("Birds", "Birds"),
-        ("Reptiles", "Reptiles"),
-        ("Others", "Others")
-    )
-    specialty = models.CharField(max_length=10, choices=SPECS)
+
+    class SpecChoices(models.TextChoices):
+        Mammal = "Mammal"
+        Birds = "Birds"
+        Reptiles = "Reptiles"
+        Others = "Others"
+
+    # SPECS = (
+    #     ("Mammals", "Mammals"),
+    #     ("Birds", "Birds"),
+    #     ("Reptiles", "Reptiles"),
+    #     ("Others", "Others")
+    # )
+    # specialty = models.CharField(max_length=10, choices=SPECS)
+    specialty = models.CharField(max_length=10, choices=SpecChoices.choices)
     managed_animals = models.ManyToManyField(to=Animal)
 
 
 class Veterinarian(Employee):
     license_number = models.CharField(max_length=10)
+
+
+class ZooDisplayAnimal(Animal):
+    class Meta:
+        proxy = True
