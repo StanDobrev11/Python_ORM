66c39fd4acfc46126a9c1eb2c6e24c79551a2e25
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-01-15 21:44:31 +0200

    update

--- orm_skeleton_lab_8/main_app/models.py
+++ orm_skeleton_lab_8/main_app/models.py
@@ -91,3 +91,17 @@ class FoodCriticRestaurantReview(RestaurantReview):
         verbose_name = 'Food Critic Review'
         verbose_name_plural = 'Food Critic Reviews'
         unique_together = ['reviewer_name', 'restaurant']
+
+
+class MenuReview(models.Model):
+    reviewer_name = models.CharField(max_length=100)
+    menu = models.ForeignKey(to=Menu, on_delete=models.CASCADE)
+    review_content = models.TextField()
+    rating = models.PositiveIntegerField(validators=[validators.MaxValueValidator(5)])
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Menu Review'
+        verbose_name_plural = 'Menu Reviews'
+        unique_together = ['reviewer_name', 'menu']
+        indexes = [models.Index(fields=['menu'], name="main_app_menu_review_menu_id")]
