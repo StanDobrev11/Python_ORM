d8fea8eb3090c71121aa5065ccd5184d5457e92d
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-01-15 20:45:39 +0200

    update

--- orm_skeleton_lab_8/main_app/models.py
+++ orm_skeleton_lab_8/main_app/models.py
@@ -76,3 +76,18 @@ class RestaurantReview(models.Model):
         verbose_name = "Restaurant Review"
         verbose_name_plural = "Restaurant Reviews"
         unique_together = ['reviewer_name', 'restaurant']
+        abstract = True
+
+
+class RegularRestaurantReview(RestaurantReview):
+    pass
+
+
+class FoodCriticRestaurantReview(RestaurantReview):
+    food_critic_cuisine_area = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ['-rating']
+        verbose_name = 'Food Critic Review'
+        verbose_name_plural = 'Food Critic Reviews'
+        unique_together = ['reviewer_name', 'restaurant']
