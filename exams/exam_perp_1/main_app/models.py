4736777371b67f7b73324fe16156508c4fd9b159
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-04-28 21:42:24 +0300

    completed task 1

---
+++ exams/exam_perp_1/main_app/models.py
@@ -0,0 +1,62 @@
+from django.core.validators import MinLengthValidator, MinValueValidator
+from django.db import models
+
+
+# Create your models here.
+class Profile(models.Model):
+    full_name = models.CharField(
+        max_length=100,
+        validators=[MinLengthValidator(limit_value=2)]
+    )
+    email = models.EmailField()
+
+    phone_number = models.CharField(
+        max_length=15,
+        help_text='This field is typically a string to accommodate various phone number formats.'
+    )
+
+    address = models.TextField(
+        help_text='This field can store longer text, suitable for addresses.'
+    )
+
+    is_active = models.BooleanField(default=True)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Product(models.Model):
+    name = models.CharField(
+        max_length=100
+    )
+
+    description = models.TextField()
+
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(limit_value=0.01)]
+    )
+
+    in_stock = models.PositiveIntegerField(
+        validators=[MinValueValidator(0)]
+    )
+
+    is_available = models.BooleanField(default=True)
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+
+class Order(models.Model):
+    profile = models.ForeignKey(to=Profile, on_delete=models.CASCADE)
+
+    products = models.ManyToManyField(to=Product)
+
+    total_price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(limit_value=0.01)]
+    )
+
+    creation_date = models.DateTimeField(auto_now_add=True)
+
+    is_completed = models.BooleanField(default=False)
