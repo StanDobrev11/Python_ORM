c36ccf8232ba1a1e1f77f5c4be6c44a56f30dffa
StanDobrev11 <134813148+StanDobrev11@users.noreply.github.com>
2024-05-05 00:01:44 +0300

    working on query 1

--- exams/exam_prep/caller.py
+++ exams/exam_prep/caller.py
@@ -7,8 +7,8 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "orm_skeleton.settings")
 django.setup()
 
 # Import your models here
-from django.db.models import Q
-from main_app.models import Director
+from django.db.models import Q, Count, Avg
+from main_app.models import Director, Actor
 
 
 # Create and run your queries within functions
@@ -45,10 +45,28 @@ def get_top_director():
     It retrieves the director with the greatest number of movies.
     """
 
-    top_director = Director.objects.get_directors_by_movies_count().order_by('full_name').first()
+    top_director = Director.objects.get_directors_by_movies_count().first()
 
     return f"Top Director: {top_director.full_name}, movies: {top_director.movies.count()}." if top_director else ""
+
+
+def get_top_actor():
+    """
+    It retrieves the starring actor with the greatest number of movies s/he starred in.
+    """
+    top_actor = Actor.objects.annotate(movies_count=Count('movies')).order_by('-movies_count', 'full_name').first()
+
+    if not top_actor or not top_actor.movies.exists():
+        return ''
+
+    average_rating = top_actor.movies.aggregate(Avg('rating')).get('rating__avg') or 0
+
+    return (f"Top Actor: {top_actor.full_name}, "
+            f"starring in movies: {', '.join(movie.title for movie in top_actor.movies.all())}, "
+            f"movies average rating: {round(average_rating, 1)}")
+
 
 # print(Director.objects.get_directors_by_movies_count())
 # print(get_directors(search_name=None, search_nationality='bu'))
 # print(get_top_director())
+print(get_top_actor())
